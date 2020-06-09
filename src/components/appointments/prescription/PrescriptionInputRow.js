ec3fb4786aad9bd6b57a7417faaf57e3a22d01b6
harshal <harshalsgore@gmail.com~>
2020-06-09 11:14:25 +0530

    prescription started

--- src/components/appointments/prescription/PrescriptionInputRow.js
+++ src/components/appointments/prescription/PrescriptionInputRow.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react'
 
 export default class PrescriptionInputRow extends Component {
+    constructor(){
+        super()
+        }
+    
     render() {
         return (
             <div className="container-fluid">
-                Input Row
+            <div className="row">
+            <div className="col-md-auto">
+                <label>{this.props.num}</label></div>
+                <div className="col">
+                <input type="text" placeholder="Medicine" class="form-control" aria-label="Sizing example input" 
+                        aria-describedby="inputGroup-sizing-sm"/>
             </div>
+            <div className="col-md-auto">
+            <div class="input-group mb-3">
+            <select class="custom-select" id="inputGroupSelect01">
+              <option selected>Brand...</option>
+              <option value="1">Brand 1</option>
+              <option value="2">Brand 2</option>
+              <option value="3">Brand 3</option>
+             </select></div>
+            </div>
+            <div className="col">
+                <input type="text" placeholder="strength" class="form-control" aria-label="Sizing example input" 
+                        aria-describedby="inputGroup-sizing-sm"/>
+            </div>
+            <div className="col-md-auto">
+            <div class="input-group mb-3">
+            <select class="custom-select" id="inputGroupSelect01">
+              <option selected>Tablet</option>
+              <option value="1">Syrup</option>
+              <option value="2">Injection</option>
+             </select></div>
+            </div>
+            <div className="col-md-auto">
+            <div class="input-group mb-3">
+            <select class="custom-select" id="inputGroupSelect01">
+              <option selected>1</option>
+              <option value="1">2</option>
+              <option value="2">3</option>
+              <option value="3">4</option>
+             </select></div>
+            </div>
+            <div className="col">
+                <input type="text" placeholder="freq" class="form-control" aria-label="Sizing example input" 
+                        aria-describedby="inputGroup-sizing-sm"/>
+            </div>
+            <div className="col-md-auto">
+            <div class="input-group mb-3">
+            <select class="custom-select" id="inputGroupSelect01">
+              <option selected>After Meal</option>
+              <option value="1">Before Meal</option>
+              <option value="2">Morning</option>
+             </select></div>
+             </div>
+             <div className="col font-weight-bold">
+                 <span className="btn btn-sm btn-secondary bg-default" onClick={this.delRow}>Del</span></div>
+            </div>
+            </div>
+            
         )
     }
 }
--- src/components/appointments/prescription/PrescriptionView.js
+++ src/components/appointments/prescription/PrescriptionView.js
@@ -15,19 +15,37 @@ export default class PrescriptionView extends Component {
         let inputRows = []
         if(this.state.rows > 0) {
             for(let i=0; i < this.state.rows; i++) {
-                const newRow = <div className="row" key={i}> <PrescriptionInputRow /> </div>
+                const newRow = <div className="row" key={i}> <PrescriptionInputRow num={i}/> </div>
                 inputRows.push(newRow)
             }
         }
+        
+
+        //var index=2
+        //if (index > -1) { 
+            //inputRows.splice(index,1);
+         // }
+
         return (
-            <div className="container text-center pt-3">
-                <div>
+            <div className="container-fluid">
+            <div className="row mt-2 mb-2">
+            <div className="col-md-auto font-weight-bold">#</div>
+            <div className="col ml-3 font-weight-bold">Medicine</div>
+            <div className="col font-weight-bold">Brand</div>
+            <div className="col font-weight-bold ">Strength</div>
+            <div className="col font-weight-bold">Package</div>
+            <div className="col font-weight-bold">Dosage</div>
+            <div className="col font-weight-bold">Freq</div>
+            <div className="col font-weight-bold">Time</div>
+            <div className="col font-weight-bold"></div>
+            </div>
+            <div>
                     {inputRows}
                 </div>
                 <div className="row">
-                    <span className="btn btn-sm btn-secondary bg-default" onClick={this.addRow}>New</span>
+                    <span className="btn btn-sm btn-secondary bg-default" onClick={this.addRow}>ADD</span>
                 </div>
             </div>
         )
-    }
+}
 }
