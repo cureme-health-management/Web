a213213183e659e743c0aa6a2043bd85acce1481
Archis Bhoir <archisbhoir2016@gmail.com>
2021-10-26 19:57:33 +0530

    Appointment provider refactor

--- src/providers/AppointmentProvider.js
+++ src/providers/AppointmentProvider.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import {getAgeFromDate} from '../utils/date'
+import React, { Component } from "react"
+import { getAgeFromDate } from "../utils/date"
 
 export const AppointmentContext = React.createContext()
 
@@ -9,56 +9,59 @@ export default class AppointmentProvider extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            message: 'hi',
+            message: "hi",
             list: [],
             currentAppointment: {},
-            loading: false
+            loading: false,
         }
         this.selectAppointment = (appt) => {
-            const prescriptionInLocalStorage = JSON.parse(localStorage.getItem('current'))
-            if (prescriptionInLocalStorage && prescriptionInLocalStorage.length !== 0) {
+            const prescriptionInLocalStorage = JSON.parse(
+                localStorage.getItem("current")
+            )
+            if (
+                prescriptionInLocalStorage &&
+                prescriptionInLocalStorage.length !== 0
+            ) {
                 //TODO * show a modal saying "There are unsaved changes"
+                return window.alert("There are unsaved changes")
             }
-            this.setState({ currentAppointment: appt }, () => localStorage.removeItem('current'))
+            this.setState({ currentAppointment: appt }, () =>
+                localStorage.removeItem("current")
+            )
         }
-    }
-
-    setList = (list) => {
-        this.setState({ list })
     }
 
     componentDidMount() {
         this.setState({ loading: true })
-        fetch('/v1.0/appointments', {
+        fetch("/v1.0/appointments", {
             method: "GET",
-            // mode: "cors",
             headers: {
-                "Authorization": `Bearer ${localStorage.token}`
-            }
+                Authorization: `Bearer ${localStorage.token}`,
+            },
         })
-            .then(response => response.json())
-            .then(response => {
-                console.log(response)
-                if (response.appointments.length > 0) {
-                    let temp = []
-                    response.appointments.map(a => {
-                        const { appointmentId, date_of_birth, first_name, last_name, gender, patientId } = a
-                        const y = date_of_birth.split('-')[0]
-                        const m = date_of_birth.split('-')[1]
-                        const d = date_of_birth.split('-')[2]
-                        temp.push({
-                            id: appointmentId,
+            .then((response) => response.json())
+            .then(({ appointments }) => {
+                if (appointments.length > 0) {
+                    const formattedAppointmets = appointments.map((a) => {
+                        const { date_of_birth, first_name, last_name } = a
+                        const y = date_of_birth.split("-")[0]
+                        const m = date_of_birth.split("-")[1]
+                        const d = date_of_birth.split("-")[2]
+                        return {
+                            ...a,
                             firstName: first_name,
                             lastName: last_name,
-                            gender: gender,
-                            age: getAgeFromDate(new Date(y, m, d))
-                        })
+                            age: getAgeFromDate(new Date(y, m, d)),
+                        }
                     })
-                    this.setState({ list: temp, currentAppointment: temp[0] });
+                    this.setState({
+                        list: formattedAppointmets,
+                        currentAppointment: formattedAppointmets[0],
+                    })
                 }
                 this.setState({ loading: false })
             })
-            .catch(err => {
+            .catch((err) => {
                 this.setState({ loading: false })
                 console.log(err)
             })
@@ -66,10 +69,12 @@ export default class AppointmentProvider extends Component {
 
     render() {
         return (
-            <AppointmentContext.Provider value={{
-                data: this.state,
-                select: this.selectAppointment
-            }} >
+            <AppointmentContext.Provider
+                value={{
+                    data: this.state,
+                    select: this.selectAppointment,
+                }}
+            >
                 {this.props.children}
             </AppointmentContext.Provider>
         )
